9cbe9f6848a448efaac7512947c43e32f86f1491
Marlon Dias <marlondias@users.noreply.github.com>
2020-09-26 20:38:17 -0300

    Model additional configuration + column metadata

--- app/Actor.php
+++ app/Actor.php
@@ -6,5 +6,44 @@ use Illuminate\Database\Eloquent\Model;
 
 class Actor extends Model
 {
-    //
+    protected $table = 'actor';
+    protected $primaryKey = 'actor_id';
+    protected $timestamps = false;
+
+    public $columnsMetadata = [
+        'first_name' => [
+            'type' => 'string',
+            'orderBy' => [ 'allowed' => true, ],
+            'filter' => [
+                'allowed' => true, 
+                'length' => [1, 100], 
+                'methods' => ['equals', 'contains'],
+            ],
+        ],
+        'last_name' => [
+            'type' => 'string',
+            'orderBy' => [
+                'allowed' => true, 
+                'default' => 'ASC'
+            ],
+            'filter' => [
+                'allowed' => true, 
+                'length' => [1, 100], 
+                'methods' => ['equals', 'contains'],
+            ],
+        ],
+        'last_update' => [
+            'type' => 'datetime',
+            'orderBy' => [
+                'allowed' => true, 
+                'default' => 'DESC'
+            ],
+            'filter' => [
+                'allowed' => true, 
+                'length' => [10, 10], //yyyy-mm-dd 
+                'methods' => ['equals', 'lessThan', 'greaterThan'],
+            ],
+        ],
+    ];
+
 }
